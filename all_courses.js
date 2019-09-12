b4bcb6c6adea72ddd44c73ed1c992858f5c6110f
Cody Shaben <codyshaben@Codys-MacBook-Air.local>
2019-09-12 17:03:48 -0600

    finalized style template

--- all_courses.js
+++ all_courses.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const courseURL = 'http://localhost:3000/api/v1/courses'
+    const playerURL = 'http://localhost:3000/api/v1/players'
+    const addCourseURL = 'http://localhost:3000/api/v1/addCourse'
 
 
     fetch(courseURL)
@@ -9,6 +11,12 @@ document.addEventListener('DOMContentLoaded', () => {
         showCourses(response)
     })
 
+    fetch(playerURL)
+    .then(response => response.json())
+    .then(response => console.log(response[0].courses))
+       
+    
+
     function showCourses(courses) {
     courses.forEach(course => {
         const courseCard = document.createElement('div')
@@ -16,10 +24,9 @@ document.addEventListener('DOMContentLoaded', () => {
         const courseLocation = document.createElement('p')
         const addCourseButton = document.createElement("button")
         const image = document.createElement("img")
-        const allCourses = document.createElement('div')
         courseName.innerText = "Course: " + course.name
         courseName.value = course.id
-        courseLocation.innerText = "Course Location: " + course.location
+        courseLocation.innerText = "Location: " + course.location
         courseLocation.value = course.id
         addCourseButton.setAttribute("id", "addCourseButton")
         addCourseButton.innerText = "Add Course"
@@ -28,19 +35,34 @@ document.addEventListener('DOMContentLoaded', () => {
         courseCard.appendChild(addCourseButton)
         courseCard.append(courseName, courseLocation, addCourseButton)
         addCourseButton.addEventListener('click', event => {
-            fetch()
+            const courseToRemove = event.target.parentNode
+            fetch(addCourseURL, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify( {
+                    player: {
+                        player_id: 1,
+                        course_id: course.id
+                    }
+                }) 
+            })
+            .then(courseToRemove.remove())
+            //where you want to put logic for adding course to player
         })
-       
         document.body.appendChild(courseCard)
-
-
-
+    })
 
    
 
+
+
+
+    
         
+    }
 
 
-    })
-}
 })
---
+++ player_homepage.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const playerURL = `http://localhost:3000/api/v1/players/`
+    const deleteCourseURL = `http://localhost:3000/api/v1/removeCourse/`
+    const viewCourseURL = 
+
+    fetch(playerURL)
+    .then(response => response.json())
+    .then(response => {
+        response.forEach(response => {             
+            showCourses(response.courses)
+            // console.log(response.id)
+        })
+    })
+
+    function showCourses(courses) {
+        courses.forEach(course => {
+            const courseCard = document.createElement('div')
+            const courseName = document.createElement('p')
+            const courseLocation = document.createElement('p')
+            const removeCourseButton = document.createElement("button")
+            const seeCourseButton = document.createElement("button")
+            const image = document.createElement("img")
+            courseName.innerText = "Course: " + course.name
+            courseName.value = course.id
+            courseLocation.innerText = "Location: " + course.location
+            courseLocation.value = course.id
+            // removeCourseButton.setAttribute("id", "removeCourseButton")
+            removeCourseButton.innerText = "Remove Course"
+            // seeCourseButton.setAttribute("id", "seeCourseButton")
+            seeCourseButton.innerText = "See Course"
+            seeCourseButton.addEventListener
+            image.src = course.image
+            courseCard.prepend(image)
+            courseCard.append(courseName, courseLocation, removeCourseButton, seeCourseButton)
+            removeCourseButton.addEventListener('click', event => {
+                const courseToRemove = event.target.parentNode
+                fetch(deleteCourseURL + course.id, {
+                    method: "DELETE",
+                    // headers: {
+                    //     'Accept': 'application/json',
+                    //     'Content-Type': 'application/json'
+                    // },
+                    // body: JSON.stringify( {
+                    //     player: {
+                    //         player_id: 1,
+                    //         course_id: course.id
+                    //     }
+                    // })
+                })  
+                .then(courseToRemove.remove())
+            })
+            document.body.appendChild(courseCard)
+    })
+
+}
+})
+
+
